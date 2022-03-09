b5a6e53c7edb3b2548aec487613b4a767e05cd40
Michael Cote <michaelcote7@outlook.com>
2022-03-08 20:39:30 -0800

    day 2 orbit controlls in good order

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-// import { useRef, useState } from 'react';
+// import { useRef } from 'react';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { Canvas, useLoader } from '@react-three/fiber';
@@ -7,27 +7,73 @@ import { Suspense } from "react";
 import CameraControls from './cameraControl';
 
 
-const Scene = () => {
-  const materials = useLoader(MTLLoader, "ex/homer_0.mtl");
-  const obj = useLoader(OBJLoader, "ex/homer_0.obj", (loader) => {
+const Loading = () => {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[10, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="black"
+        transparent
+        opacity={0.1}
+        roughness={0}
+        metalness={50}
+        wireframe={true}
+      />
+    </mesh>
+  );
+}
+
+
+
+const Temple = () => {
+  const materials = useLoader(MTLLoader, "temple/blueibodi.mtl");
+  const obj = useLoader(OBJLoader, "temple/blueibodi.obj", (loader) => {
     materials.preload()
     loader.setMaterials(materials)
   });
-
+  
+  // const ref = useRef(obj);
+  // useFrame(() => (ref.current.rotation.y += 0.005)); // auto-rotate set on camera instead 
   console.log(obj);
-  return <primitive object={obj} scale={1}/>;
+                                                    // +- [x,y,z]
+  return <primitive object={obj} scale={1} position={[0, -10, 0]}/>;
 };
+
+
+// const Homer = () => {
+//   const materials = useLoader(MTLLoader, "ex/homer_0.mtl");
+//   const obj = useLoader(OBJLoader, "ex/homer_0.obj", (loader) => {
+//     materials.preload()
+//     loader.setMaterials(materials)
+//   });
+
+//   console.log(obj);
+//                                                       // +- [x,y,z]
+//   return <primitive object={obj} scale={1} position={[0,0,0]}/>;
+// }
+
 
 const App = () => {
   return (
     <div className="flex">
       <div className="cont">
-        <Canvas className='can-can'>
-          <Suspense fallback={null}>
+        <Canvas className='can-can' orthographic camera={{ zoom: 18, position: [0, 25, 100] }}>
+          
+          <ambientLight intensity={0.5} />
+          {/* <directionalLight /> */}
+          
+          <Suspense fallback={<Loading/>}>
+            <axesHelper />
+            
             <CameraControls />
-            <ambientLight intensity={0.5} />
-            <directionalLight/>
-            <Scene />
+            
+            <spotLight position={[10, 20, 10]} angle={0.15} penumbra={5} />
+            <pointLight position={[-10, -10, -10]} />
+            
+            <Temple />
+            {/* <Homer /> */}
+            
           </Suspense>
         </Canvas>
       </div>
@@ -36,3 +82,6 @@ const App = () => {
 }
 
 export default App;
+
+// for <Canvas />
+// perspective camera={{ zoom: 1, position: [0, 25, 10]
--- src/cameraControl.jsx
+++ src/cameraControl.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 import { useFrame, useThree, extend } from '@react-three/fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
 extend({ OrbitControls });
 const CameraControls = () => {
   const {
@@ -8,10 +10,44 @@ const CameraControls = () => {
     gl: { domElement },
   } = useThree();
   const controls = useRef();
-  
-  // @ts-ignore
+
   useFrame(() => controls.current.update());
-  // @ts-ignore
-  return <orbitControls ref={controls} args={[camera, domElement]} />;
+
+  // setTimeout(() => {
+  //   controls.current.reset(); // to reset cam...
+  // }, 5000);
+
+
+  return <orbitControls 
+      ref={controls} 
+      args={[camera, domElement]} 
+      makeDefault
+      enableZoom={true}
+      enableDamping={true}
+      dampingFactor={0.05}
+      enablePan={false}
+      minZoom={10}
+      maxZoom={30}
+      maxPolarAngle={Math.PI / 2} // dont show bottom
+      autoRotate={true}
+      autoRotateSpeed={2.5}
+      />;
 };
 export default CameraControls;
+
+
+
+
+
+
+// orbitcontrols>
+// POLAR = TOP/BOTTOM
+// minPolarAngle={-Math.PI}
+// maxPolarAngle={Math.PI}
+// AZIMUTH = LEFT/RIGHT (not working if auto-rotating)
+// minAzimuthAngle={-Math.PI / 4}
+// maxAzimuthAngle={Math.PI / 4}
+
+// zoom for perspective cam only
+// maxDistance={20}
+// minDistance={10}
